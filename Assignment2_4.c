f276b0258c64948afdf7cdf8f7fe94d84cd4085b
tejas01112000 <119598328+tejas01112000@users.noreply.github.com>
2023-07-20 23:31:18 +0530

    Add files via upload

---
+++ Assignment2_4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+#include<sys/stat.h>
+#include<dirent.h>
+
+int main(int argc, char *argv[])
+{
+    DIR *dp = NULL;
+    struct dirent * entry = NULL;
+
+    if(argc != 2)
+    {
+        printf("Insufficient arguments\n");
+        return -1;
+    }
+
+    dp = opendir(argv[1]);
+    if(dp == NULL)
+    {
+        printf("Unable to open directory\n");
+        return -1;
+    }
+    
+    printf("Entries from the directory file are : \n");
+    
+    printf("Inode Number \t\t File Name\n");
+
+    while((entry = readdir(dp)) != NULL)
+    {
+        printf("%20s : %d \n",entry->d_name, entry->d_ino);
+    }
+    closedir(dp);
+    return 0;
+}
